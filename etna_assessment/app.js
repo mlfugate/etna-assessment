e8d5c04d5b93ad07e0787edc8dd543842a2047f4
Meagan F <66763746+mlfugate@users.noreply.github.com>
2021-06-21 09:54:18 -0700

    Update app.js

--- etna_assessment/app.js
+++ etna_assessment/app.js
@@ -1,26 +1,3 @@
-// $(document).ready(function(){
-//     $(".fa-bars").click(function() {
-//       $(".burger-menu").addClass("active");
-//       console.log('clicked')
-//     });
-//   });
-// $(document).ready(function(){
-//     $(".fa-bars").click(function(){
-//         console.log('clicked')
-//       $(".burger-menu").addClass('active');
-//     });
-//   });
-
-
-//   const navClickActive = () => {
-//       $('.fa-bars').addClass('active')
-//       $(".burger-menu").addClass("active")
-
-//       console.log('clicked')
-
-//   }
-
-//   $('.fa-bars').click(navClickActive)
 
 function onMenuClick(e) {
 e.preventDefault()
